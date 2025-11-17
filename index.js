d41aa58fd6d43fc94607b63d85111acfc19ed436
Gokila-M <gokila1305@gmail.com>
2025-11-17 14:09:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose'
+import dotenv from "dotenv"
+import cors from "cors"
+    
+dotenv.config()
+
+const app= express();
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/HRMSDB')
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB... '+err.message));
+  
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+
+const PORT = process.env.PORT || 7800;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
