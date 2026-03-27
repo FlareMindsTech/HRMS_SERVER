4f3697d93ab7343efe5d10881bd864c25cc9dd48
karthikeyan0704 <karthikeyangnanasekar07@gmail.com>
2026-03-27 12:55:52 +0530

    owner route uncommand

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { deleteUser, getAllUser, getNoOwner, getUserById, login, logout, employeeInternReg, managementReg, profile, Register, updateUser } from "../Controller/UserController.js";
+import { deleteUser, getAllUser, getNoOwner, getUserById, login, logout, employeeInternReg, managementReg, profile, Register, updateUser, ownerReg } from "../Controller/UserController.js";
 import { Authendication } from "../Middleware/Auth.js";
 const router = express.Router();
 
 router.post("/login",login)
 router.post("/logout",Authendication,logout)
 router.get("/profile",Authendication,profile)
-// router.post("/v1/ownerReg",ownerReg)
+router.post("/v1/ownerReg",ownerReg)
 // router.delete("/v2/currentCompany",currentCompany)
 
 router.post("/v2/reg",  Register)
